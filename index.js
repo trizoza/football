3394444b231bf3cdbb7f70ec3747ea50aeb5da5a
Peter Trizuliak <petertrizuliak@ip-10-2-0-236.eu-west-1.compute.internal>
2019-06-14 17:58:54 +0100

    log out all matches

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const season = require('./season.json')
 
 console.log('season ', season)
+
+const ranking = []
+
+season.rounds.forEach(round => {
+    // console.log('round ', round)
+    round.matches.forEach(match => {
+        console.log('match ', match)
+    })
+})
