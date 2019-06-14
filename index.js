6e15a0bc09c2e8270115697ade23797b0b4ed637
Peter Trizuliak <petertrizuliak@ip-10-2-0-236.eu-west-1.compute.internal>
2019-06-14 19:27:27 +0100

    refactor

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
 const season = require('./season.json')
-
-// console.log('season ', season)
-
 const teams = {}
+let rankings = []
 
-const run = () => {
+const calculateRankings = () => {
     season.rounds.forEach(round => {
-        // console.log('round ', round)
         round.matches.forEach(match => {
-            // console.log('match ', match)
-            // console.log('teams ', teams)
-            Object.keys(teams).indexOf(match.team1.code) === -1 ? firstMatch(match, 1) : otherMatch(match, 1)
-            Object.keys(teams).indexOf(match.team2.code) === -1 ? firstMatch(match, 2) : otherMatch(match, 2)
+            processMatch(match, 1) 
+            processMatch(match, 2)
         })
     })
-
-    const rankings = sortTeams(teams)
-
-    console.log('rankings ', rankings)
+    return sortTeams(teams)
 }
 
-const firstMatch = (match, pos) => {
-    
+const processMatch = (match, pos) => {
     let teamCode = match[`team${pos}`].code
-    
-    console.log('teamCode ', teamCode)
-    
     let invertedPos = pos === 1 ? 2 : 1
+    let matchDetails = {
+        match, 
+        teamCode, 
+        pos, 
+        invertedPos
+    }
+    !teams[teamCode] ? createTeamRecord(matchDetails) : updateTeamRecord(matchDetails)
+}
 
-    team = {
+const createTeamRecord = ({match, teamCode, pos, invertedPos}) => {
+    let team = {
         teamName: match[`team${pos}`].name,
         wins: match[`score${pos}`] > match[`score${invertedPos}`] ? 1 : 0,
         draws: match[`score${pos}`] === match[`score${invertedPos}`] ? 1 : 0,
@@ -37,17 +34,11 @@ const firstMatch = (match, pos) => {
         goalsAgainst: match[`score${invertedPos}`],
         points: calculatePoints(match[`score${pos}`], match[`score${invertedPos}`]),
     }
-    
-    // rank: "",   // udpateRank()
     team.goalDifference = team.goalsFor - team.goalsAgainst,
     teams[teamCode] = team
-    // console.log('teams ', teams)
-    // console.log('teams ', Object.keys(teams).length)
 }
 
-const otherMatch = (match, pos) => {
-    let teamCode = match[`team${pos}`].code
-    let invertedPos = pos === 1 ? 2 : 1
+const updateTeamRecord = ({match, teamCode, pos, invertedPos}) => {
     let team = teams[teamCode]    
     let { wins, draws, losses, goalsFor, goalsAgainst, points, teamName } = team
     wins += match[`score${pos}`] > match[`score${invertedPos}`] ? 1 : 0
@@ -66,9 +57,6 @@ const otherMatch = (match, pos) => {
         points,
         goalDifference: goalsFor - goalsAgainst,
     }
-    // console.log('team before update ', team)
-    // console.log('match ', match)
-    // console.log('team after update ', updatedTeam)
     teams[teamCode] = updatedTeam
 }
 
@@ -84,26 +72,24 @@ const calculatePoints = (score1, score2) => {
 
 const sortTeams = (teams) => {
     let teamsArray = Object.keys(teams).map(teamCode => teams[teamCode])
-    console.log('teamsArray ', teamsArray)
     teamsArray.sort((team1, team2) => {
-
+        // sort by points
         if (team1.points > team2.points) return -1;
         if (team1.points < team2.points) return 1;
-    
+        // sort by goalDifference
         if (team1.goalDifference > team2.goalDifference) return -1;
         if (team1.goalDifference < team2.goalDifference) return 1;
-        
+        // sort by goalsFor
         if (team1.goalsFor > team2.goalsFor) return -1;
         if (team1.goalsFor < team2.goalsFor) return 1;
     })
-
-    console.log('teamsArray sorted ', teamsArray);
-
+    // assign ranks
     return teamsArray.map((team, index) => {
         team.rank = index + 1
         return team
     })
 }
 
+rankings = calculateRankings()
 
-run()
+console.log('rankings ', rankings)
